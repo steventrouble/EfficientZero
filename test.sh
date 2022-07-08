06eba1a9778dec3cf32d237cb9c9526262b4cbcf
steventrouble <steventrouble@gmail.com>
2022-07-08 14:04:44 -0700

    Update test script to use the correct breakout env

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -ex
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES=0
 
-python main.py --env BreakoutNoFrameskip-v4 --case atari --opr test --seed 0 --num_gpus 1 --num_cpus 20 --force \
+python main.py --env 'ALE/Breakout-v5' --case atari --opr test --seed 0 --num_gpus 1 --num_cpus 20 --force \
   --test_episodes 32 \
   --load_model \
   --amp_type 'torch_amp' \
