1aec779393a77acb86078e102b5e96fffd639fb4
Steven Weiss <sweiss@ip-172-31-32-196.ec2.internal>
2023-03-14 17:42:19 +0000

    Update train.sh to auto-populate some params from env.
    This script still requires a lot of manual tweaking. Some day it may
    be all automatic.

--- train.sh
+++ train.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
 set -ex
-export CUDA_DEVICE_ORDER='PCI_BUS_ID'
-export CUDA_VISIBLE_DEVICES=0
+NUM_GPUS="${SLURM_GPUS_ON_NODE:-1}"
+NUM_CPUS="${SLURM_CPUS_ON_NODE:-8}"
 
-python main.py --env 'ALE/Breakout-v5' --case atari --opr train --force \
-  --num_gpus 1 --num_cpus 4 --cpu_actor 1 --gpu_actor 1 \
+# To set these values, start the program following these instructions:
+#
+#  1. Leave CPU actors at 1, and increase GPU actors until GPU full according
+#     to `ray satus`
+#  2. Increase CPU actors until CPU full.
+GPU_ACTORS=140
+CPU_ACTORS=40
+
+#py-spy top --subprocesses -- \
+python3.8 main.py --env 'ALE/Breakout-v5' --case atari --opr train --force \
+  --num_gpus "$NUM_GPUS" --num_cpus "$NUM_CPUS" --gpu_mem 40 \
+  --cpu_actor $CPU_ACTORS --gpu_actor $GPU_ACTORS \
   --seed 0 \
   --use_priority \
   --use_max_priority \
